04d5f725848c515f114d9b044155c82fb932331a
teacherIan <ian_malloy@ssis.asia>
2023-04-11 12:50:19 +0800

    Second Day

--- main.js
+++ main.js
@@ -12,6 +12,21 @@ function clickHandler(e) {
 
   nameArr.push(formData.get('name'));
   ageArr.push(formData.get('age'));
+
+  // viewNameArr();
+  // viewAgeArr();
 }
 
 submitButton.addEventListener('click', clickHandler);
+
+function viewNameArr() {
+  for (let i = 0; i < nameArr.length; i++) {
+    console.log(nameArr[i]);
+  }
+}
+
+function viewAgeArr() {
+  ageArr.forEach((age) => {
+    console.log(age);
+  });
+}
